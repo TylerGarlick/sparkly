295f7c2917d780dd71cce7adb04cb5443a0733bf
Tyler Garlick <tjgarlick@gmail.com>
2017-08-14 17:24:28 -0600

    Hook up apollo client
    - Added graphql stuff

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import { ApolloProvider } from 'react-apollo'
+import currentTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
 import ApolloClient from './config/graphql'
 
 
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+injectTapEventPlugin()
+
+
 ReactDOM.render((
   <ApolloProvider client={ApolloClient}>
-    <App />
+    <MuiThemeProvider muiTheme={getMuiTheme(currentTheme)}>
+      <App />
+    </MuiThemeProvider>
   </ApolloProvider>
 ), document.getElementById('root'))
 registerServiceWorker()
